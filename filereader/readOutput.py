e59858ab3c21506ba0076f8b936b0668bd005d08
root <root@kali>
2019-02-09 20:43:15 +1100

    filereader changed

--- filereader/readOutput.py
+++ filereader/readOutput.py
@@ -1,14 +1,38 @@
 import time;
 
+def printArr(instance):
+    for cell in instance:
+        for celll in cell:
+            print(celll + "/");
+        print("\n");
+    print("///////////////////////////////////////");
+    return;
+
+#beginingOfTable = "Station MAC".strip();
+
 while True:
-    instanceOfData = [];
-    with open("/root/wifiData-01.csv","r") as f:
+    curInstanceOfData = []; #current
+    hisInstanceOfData = []; #old
+    flag = False;
+    with open("/root/myOutput-01.csv","r") as f:
         for line in f:
-            if line is not "\n":
-                instanceOfData.append(line);
-  
+            if line is not "\n":   #ignore empty lines             
+                cells = line.split(",");
+                i=0;
+                length = len(cells);
+                while i < length:#go trough sliced line and remove white spaces
+                    cells[i] = cells[i].strip();
+                    i=i+1;
+                if cells[0].startswith("Station MAC"):#if old table switch container
+                    flag = True;
+                if flag:#check what table should put info
+                    hisInstanceOfData.append(cells);
+                else:
+                    curInstanceOfData.append(cells);
+    #printArr(curInstanceOfData);
+    #printArr(hisInstanceOfData);
+
     f.close();
-    #print(instanceOfData);
-    for cell in instanceOfData:
-        print(cell + "\n");
+    #remove for infinite loop
+    #time.sleep(60);
     break;
