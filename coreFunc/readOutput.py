6a5756d2c93ad7b70ae0862e70bd076f5f708ac1
root <root@kali>
2019-04-02 10:00:23 +1100

    network start

--- coreFunc/readOutput.py
+++ coreFunc/readOutput.py
@@ -1,4 +1,5 @@
 import time;
+import datetime;
 import os;
 import math;
 import json;
@@ -32,7 +33,7 @@ def getChannel():
         if splitWords[0][0] is '0':
             splitWords[0] = splitWords[0][1:];
         result.append(splitWords);
-    
+
     return result;
 
 #fspl algorithm (km)
@@ -53,29 +54,33 @@ def calculateDistance(f,fspl):
 def fetchData():
     wifiInstanceOfData = []; #wifi
     channelFreq = getChannel();
-    with open("/root/Output-01.csv","r") as f:
-        for line in f:
-            if line is not "\n":   #ignore empty lines             
-                cells = line.split(",");                
-                
-                i = len(cells) - 1;#length
-                length = i;
-                while i >= 0:#go trough sliced line and remove white spaces
+    try:
+        #with open("Output-01.csv","r") as f:
+        with open("/root/Output-01.csv","r") as f:
+            for line in f:
+                if line is not "\n":   #ignore empty lines
+                    cells = line.split(",");
 
-                    if i == 0 or  i == 8 or i==13: #0 mac #7 channel #8 pwr #13 essid
-                        cells[i] = cells[i].strip();
-                    elif i == 3:
-                        tmp = cells[i].strip();
-                        for ch in channelFreq:
-                            if ch[0] == tmp:
-                                cells[i] = ch[1];
-                                break;
-                    else:
-                        cells.pop(i);
-                    i=i-1;
+                    i = len(cells) - 1;#length
+                    length = i;
+                    while i >= 0:#go trough sliced line and remove white spaces
 
-                if length == 14:#check what table should put info
-                    wifiInstanceOfData.append(cells);
+                        if i == 0 or  i == 8 or i==13: #0 mac #7 channel #8 pwr #13 essid
+                            cells[i] = cells[i].strip();
+                        elif i == 3:
+                            tmp = cells[i].strip();
+                            for ch in channelFreq:
+                                if ch[0] == tmp:
+                                    cells[i] = ch[1];
+                                    break;
+                        else:
+                            cells.pop(i);
+                        i=i-1;
+
+                    if length == 14:#check what table should put info
+                        wifiInstanceOfData.append(cells);
+    except OSError:
+        return;
 
     f.close();
     if not wifiInstanceOfData:
@@ -85,24 +90,27 @@ def fetchData():
     i = len(wifiInstanceOfData)-1;
     while i!= -1:
         if wifiInstanceOfData[i][1].strip() == '-1' or wifiInstanceOfData[i][2].strip() == '-1':
-            del wifiInstanceOfData[i];           
+            del wifiInstanceOfData[i]         
         else:
             dist = calculateDistance(wifiInstanceOfData[i][1],wifiInstanceOfData[i][2]);
             wifiInstanceOfData[i].pop(1);
-            wifiInstanceOfData[i].pop(1);
             wifiInstanceOfData[i].append(dist);
         i = i - 1;
-    #printArr(wifiInstanceOfData);
+
     try:
         postRequest(wifiInstanceOfData);
     except:
-        return;
+       return;
+
 def postRequest(instance):
-    params = json.dumps(instance).encode('utf8');
-    url = 'http://iolab.sk:8033/~venczel/jsonTest.php';
+    instance.append(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"));
+    cell =[];
+    cell.append(instance);
+    params = json.dumps(cell).encode('utf8');
+    url = 'http://iolab.sk:8033/~venczel/jsonParser/jsonPost.php';
     req = urllib.request.Request(url,data=params,headers={'content-type': 'application/json'});
     response = urllib.request.urlopen(req);
-    #print(response.read().decode('utf8'));
+    print(response.read().decode('utf8'));
 
 
 
--- startup/airdump.py
+++ startup/airdump.py
@@ -5,4 +5,4 @@ os.system("rm /root/Output-01.csv");
 os.system("iw phy phy0 interface add mon0 type monitor");
 os.system("ifconfig mon0 up");
 
-os.system("sudo airodump-ng -w Output --output-format csv mon0");
+os.system("airodump-ng -w Output --output-format csv mon0");
